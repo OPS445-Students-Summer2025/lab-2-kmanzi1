22b288734637c1520693b764df560113f91e47f9
kmanzi1 <kmanzi1@myseneca.ca>
2025-05-23 15:34:10 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Manzi Kevin
+# Author ID: 183250232
+# Date Created: 2025/05/23
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
